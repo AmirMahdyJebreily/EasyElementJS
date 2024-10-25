60691d5a8c9e40c63a1165781f3dfb0ab7d5cdb4
Amir JCS <amirmahdyjdx@gmail.com>
2024-10-26 00:31:33 +0330

    feat: add element as shadow

--- easy-element.ts
+++ easy-element.ts
@@ -1,1 +1,14 @@
-const arr: readonly number[] = [1, 2, 3]
+export class ShadowElem extends HTMLElement {
+    constructor(innerShadowHTML: string) {
+        super();
+        this.attachShadow({ mode: "open" });
+
+        let shadow = this.shadowRoot;
+        if(shadow) shadow.innerHTML = innerShadowHTML;
+    }
+}
+
+export function ElementAsShadow(elem: HTMLElement): HTMLElement {
+    return (new ShadowElem(elem.innerHTML)) as HTMLElement;
+}
+
